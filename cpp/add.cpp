c5825f99883b3e6f7474420b89cfc8d17b08d7e4
d.shipilov <d.shipilov@vk.team>
2024-10-16 18:06:18 +0300

    call c function from go

---
+++ cpp/add.cpp
@@ -0,0 +1,7 @@
+#include "include/add.h"
+
+extern "C" {
+int add_nums(int a, int b) {
+    return a + b;
+}
+}
---
+++ hello/add.h
@@ -0,0 +1,17 @@
+#ifndef ADD_H
+#define ADD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add_nums(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+#endif
